ee3f35a956170e8d69af5c9931bd9b87f675d1ec
Steffi <steffi_grote@eva.mpg.de>
2016-12-07 18:04:54 +0100

    Add get_anno_genes and usage_examples.R

---
+++ FuncBlocks/R/get_anno_genes.R
@@ -0,0 +1,119 @@
+
+
+# return a dataframe with genes annotated to (enriched) GOs 
+
+# input: result list produced by go_enrich, optional: FWER, background
+# alternative input: go_ids, optional: genes
+
+# output: go_id, gene, (FWER, candiate/score)
+
+
+get_anno_genes = function(res, fwer_threshold=0.05, background=FALSE, go_ids=NULL, genes=NULL){
+	
+	## Check input	
+	# given res as input:
+	if(!(missing(res))){
+		## checks
+		# check that it is really a res-object
+		if(!is.list(res) && all(names(res) == c("results","genes"))){
+			stop("Please use an object returned from go_enrich as input (list with 2 elements).\n Alternatively go_ids need to be defined.")
+		}
+		if(!is.null(go_ids)){
+			warning("Unused argument: structure_ids")
+		}		
+		if(!is.null(genes)){
+			warning("Unused argument: genes")
+		}
+		## get significant GOs given the fwer_threshold
+		message("extract enriched GOs...")
+		fwers = res[[1]]
+		go_ids = fwers[fwers[,7] < fwer_threshold, "node_id"]
+		if(length(go_ids) == 0){
+			stop(paste("No significantly enriched GOs at FWER-threshold ",fwer_threshold,sep=""))
+		}
+		## get genes
+		res_genes = res[[2]]
+	# without res as input	
+	} else {
+		## checks
+		# GO-IDs
+		if(is.null(go_ids)){
+			stop("Please define go_ids.")
+		}
+		# background
+		if(background){
+			warning("Unused argument: background")
+		}
+	}
+
+	## GO-graph and GO-annotations	
+	# find children
+	message("find child nodes of GOs...")
+	children = list()
+	for(go in go_ids){
+		# go_id in ontology
+		go_id_term = term[term[,4]==go, 1]
+		if(length(go_id_term) == 0){ 
+			next
+		}	
+		# go-categories that are children of *go* (cols graph_path: id, parent, child, relation, distance, ...)
+		go_children = graph_path[graph_path[,2]==go_id_term,]
+		# GO-IDs of child nodes (and the node itself)
+		child_gos = term[term[,1] %in% go_children[,3],4]
+		children[[go]] = child_gos
+	}
+	
+	# 3) find genes annotated to child nodes
+	message("find genes annotated to child nodes...")
+	# remove obsolete terms
+	term = term[term[,5]==0,]
+	# remove empty genes in annotations
+	go_anno = go_anno[go_anno[,2]!="",]
+	# restrict annotations to GOs present in term
+	anno = go_anno[go_anno[,1] %in% term[,4],]
+	# restrict to genes
+	if (!missing(res)){
+		if(!background && all(res_genes %in% c(0,1))){ # reduce to candidate genes (hyper, background=FALSE)
+			genes = names(res_genes[res_genes==1])
+		} else if(background & all(res_genes==1)){ # dont reduce (hyper, backgr=TRUE, no bg defined)
+			genes = NULL
+		} else { # reduce to input genes (hyper, background=TRUE and defined, wilcoxon)
+			genes = names(res_genes)
+		}
+	}
+	if(!is.null(genes)){
+		anno = anno[anno[,2] %in% genes, ]  
+	}
+	if(nrow(anno)==0){
+		warning("No GO-annotations found for the input genes.")
+		return(NULL)
+	}
+	# restrict to child-GOs
+	child_anno = lapply(children, function(x) unique(anno[anno[,1] %in% x,2]))
+	# rearrange to dataframe
+	message("rearrange output...")
+	go_id = lapply(seq_along(child_anno), function(i) rep(names(child_anno)[i], length(child_anno[[i]])))
+	go_id = unlist(go_id)
+	if(length(go_id)==0){
+		warning("No GO-annotations found for the input.")
+		return(NULL)
+	}
+	anno_gene = unlist(child_anno)
+	out = data.frame(go_id, anno_gene)
+	
+	if(!(missing(res))){
+		FWER = fwers[match(out[,1], fwers[,2]),7]
+		score = res_genes[as.character(out$anno_gene)]
+		# replace NA with 0 for background genes
+		score[is.na(score)] = 0
+		out = cbind(out, FWER, score)		
+		out = out[order(out$FWER, out$go_id, out$score, out$anno_gene),]
+	} else {
+		out = out[order(out$go_id, out$anno_gene),]
+	}
+		
+	row.names(out) = 1:nrow(out)
+	message("Done.")
+
+	return(out)
+}	
---
+++ usage_examples.R
@@ -0,0 +1,93 @@
+
+set.seed(123)
+
+###### standard go-enrichment with 13 candidate genes
+library(FuncBlocks)
+
+## input: a vactor with '1' for candidate and optional '0' for background genes
+## the names of the vector are the corresponding gene symbols	
+genes = rep(1, 13)
+names(genes) = c('NCAPG', 'APOL4', 'NGFR', 'NXPH4', 'C21orf59', 'CACNG2', 'AGTR1', 'ANO1', 
+ 'BTBD3', 'MTUS1', 'CALB1', 'GYG1', 'PAX2')
+
+# standard analysis with 1000 randomsets 
+go_res = go_enrich(genes)
+# the output is a list of 2 elements: the results from the anlysis and the input genes 
+head(go_res[[1]]) # FWER_overrep is the most interesting
+go_res[[2]] 
+
+
+
+#### variations
+
+# random gene selection dependent on gene-length (for FWER randomsets)
+go_len = go_enrich(genes, gene_len=TRUE)
+
+# different number of randomsets
+go_less_ran = go_enrich(genes, n_randsets=100)
+
+# scores for genes and wilcoxon rank sum test
+genes = sample(1:30, 13)
+names(genes) = c('NCAPG', 'APOL4', 'NGFR', 'NXPH4', 'C21orf59', 'CACNG2', 'AGTR1', 'ANO1', 
+ 'BTBD3', 'MTUS1', 'CALB1', 'GYG1', 'PAX2')
+go_willi = go_enrich(genes, test='wilcoxon', n_randsets=100)
+
+
+# regions as input
+genes = c(1, rep(0,6))
+names(genes) = c('8:82000000-83000000', '7:1300000-56800000', '7:74900000-148700000',
+ '8:7400000-44300000', '8:47600000-146300000', '9:0-39200000', '9:69700000-140200000')
+go_region = go_enrich(genes, n_randsets=100)
+
+# background regions only on a circularized chromosome
+go_region_circ = go_enrich(genes, n_randsets=100, circ_chrom=TRUE)
+
+
+
+####### GO-graph functions (more to come)
+
+#### (1) get gene annotations
+
+## A) given a go_enrich result for GO-IDs with FWER<0.05 (FWER for overrepresentation / high_rank)
+anno = get_anno_genes(go_res)
+# using a different FWER-threshold
+anno_willi = get_anno_genes(go_willi, fwer_threshold=0.2) 
+# 'score' refers to candidate(1) or background(0) gene (or score in the wilcoxon test)
+head(anno)
+head(anno_willi)
+
+# including background genes and using a different FWER-threshold
+anno_bg = get_anno_genes(go_res, background=TRUE)
+head(anno_bg)
+
+# also works with genomic regions as input
+anno_region = get_anno_genes(go_region, fwer_threshold=0.1)
+
+
+## B) given GOs and "optionally" genes directly
+gos = c('GO:0072025','GO:0072221','GO:0072205','GO:0072235')
+genes = c('NCAPG', 'APOL4', 'NGFR', 'NXPH4', 'C21orf59', 'CACNG2', 'AGTR1', 'ANO1', 
+ 'BTBD3', 'MTUS1', 'CALB1', 'GYG1', 'PAX2')
+anno_genes = get_anno_genes(go_ids=gos, genes=genes)
+
+# get all genes annotated to these GOs
+anno_all = get_anno_genes(go_ids=gos)
+
+# does not work with regions 
+# (maybe 'yet', but its easy to get genes from regions and use those)
+# (go_region[[2]] e.g. has all genes contained in the input regions for that go_enrich analysis)
+
+
+
+#### (2) get name 
+get_GO_names(c("GO:0051082", "GO:123", "GO:0042254", "GO:0000109"))
+
+
+
+
+
+
+
+
+
+
