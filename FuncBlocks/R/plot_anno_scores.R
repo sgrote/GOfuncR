824a18fc5d7983398691e7deb1ea17df8525bc6c
Steffi <steffi_grote@eva.mpg.de>
2017-09-07 11:31:06 +0200

    rearrange plot_anno_scores

--- FuncBlocks/R/plot_anno_scores.R
+++ FuncBlocks/R/plot_anno_scores.R
@@ -31,73 +31,84 @@ plot_anno_scores = function(res, go_ids){
 	pie_cols = c("#F15A60","#7BC36A","#599BD3","#F9A75B","#9E67AB","#CE7058","#D77FB4")
 	root_cols = data.frame(root=def_root_ids, col=pie_cols[1:length(def_root_ids)], stringsAsFactors=FALSE)
 	
-	# check if background genes are defined (optional for hyper)
-	if (test == "hyper" & all(in_genes[,2] == 1)){
-		bgdef = FALSE
-	} else {
-		bgdef = TRUE
-	}
 	# just in case
 	go_ids = as.character(go_ids)
 	# keep order of input GO's (which gets messed up in get_anno_genes by *apply)
 	ordere = data.frame(go_ids, rank=1:length(go_ids))
 	
-	# get IDs for root_nodes
-	root_names = unique(res[[1]][,1])
-	root_names = term[match(root_names, term[,2]) ,]
-	root_ids = root_names[,4]	
-	
-	# get annotation for go_ids and root-nodes
-	if (bgdef){
-		# background defined: restrict to input genes
-		anno = get_anno_genes(go_id=go_ids, genes=in_genes[,1], ref_genome=res[[3]])
-		root_anno = get_anno_genes(go_id=root_ids, genes=in_genes[,1], ref_genome=res[[3]])
+	# check if background genes are defined (optional for hyper)
+	if (test == "hyper" & all(in_genes[,2] == 1)){
+		genes = NULL
 	} else {
-		# background not defined: get all genes annotations
-		anno = get_anno_genes(go_id=go_ids, ref_genome=res[[3]])
-		root_anno = get_anno_genes(go_id=root_ids, ref_genome=res[[3]])
+		genes = in_genes[,1]
 	}
-	if (is.null(anno)) return(invisible(anno)) # no annotations - warning from get_anno_genes
 	
+	# get annotation for nodes
+	anno = get_anno_genes(go_id=go_ids, genes=genes, ref_genome=res[[3]])
+	if (is.null(anno)) return(invisible(anno)) # no annotations - warning from get_anno_genes
+	# add scores to nodes
+	anno_scores = cbind(anno, in_genes[match(anno[,2], in_genes[,1]), 2:ncol(in_genes)])
+
+	# aggregate scores in nodes (wilcox: plot score distribution)
 	if (test != "wilcoxon"){
-		# add scores to nodes and root-nodes
-		anno_scores = cbind(anno, in_genes[match(anno[,2], in_genes[,1]), 2:ncol(in_genes)])
-		root_anno_scores = cbind(root_anno, in_genes[match(root_anno[,2], in_genes[,1]), 2:ncol(in_genes)])
-		if (!bgdef){
-			anno_scores[is.na(anno_scores[,3]), 3] = 0 # default 0 for hyper
-			root_anno_scores[is.na(root_anno_scores[,3]), 3] = 0
-		}
-		# aggregate scores in nodes and root-nodes
 		if (test == "hyper"){ 
 			# counts of 1 and 0 genes in a node
-			aggrego = tapply(anno_scores[,3], anno_scores[,1], function(x) c(sum(x), length(x)-sum(x)))
-			aggrego = data.frame(go_id = names(aggrego), do.call(rbind, aggrego))
-			root_aggrego = tapply(root_anno_scores[,3], root_anno_scores[,1], function(x) c(sum(x), length(x)-sum(x)))
-			root_aggrego = data.frame(go_id = names(root_aggrego), do.call(rbind, root_aggrego))
+			anno_scores[is.na(anno_scores[,3]), 3] = 0 # default 0 for hyper (NA if background not defined)
+			anno_scores = tapply(anno_scores[,3], anno_scores[,1], function(x) c(sum(x[]), length(x)-sum(x)))
+			anno_scores = data.frame(go_id = names(anno_scores), do.call(rbind, anno_scores))
 		} else { 
-			# sums of scores in a node
-			aggrego = aggregate(anno_scores[,3:ncol(anno_scores)], list(go_id=anno_scores[,1]), sum)
-			root_aggrego = aggregate(root_anno_scores[,3:ncol(root_anno_scores)], list(go_id=root_anno_scores[,1]), sum)
+			# sums of scores in a node (binom + conti)
+			anno_scores = aggregate(anno_scores[,3:ncol(anno_scores)], list(go_id=anno_scores[,1]), sum)
 		}
-		# add colors and root_node_name
-		root_aggrego$root_name = get_names(root_aggrego[,1])[,2]
-		root_aggrego$root_col = root_cols[match(root_aggrego[,1], root_cols[,1]), 2]
-		# merge nodes with root node info
-		matched_root_name = get_names(aggrego[,1])[,3] # get names
-		aggrego$root_id = root_names[match(matched_root_name, root_names[,2]), 4]
-		aggrego = cbind(aggrego, root_aggrego[match(aggrego$root_id, root_aggrego[,1]), 2:ncol(root_aggrego)])
 	}
-	# get original order
-	aggrego = aggrego[order(ordere[match(aggrego$go_id, ordere$go_ids), 2]),]
-	rownames(aggrego) = 1:nrow(aggrego)
 	
+	# get annotation for root nodes	(conti independent of root nodes)
+	if (test != "contingency"){
+		
+		# get IDs for root_nodes
+		root_names = unique(res[[1]][,1])
+		root_names = term[match(root_names, term[,2]) ,]
+		root_ids = root_names[,4]
+	
+		# get annotation for root nodes
+		root_anno = get_anno_genes(go_id=root_ids, genes=genes, ref_genome=res[[3]])
+		# add scores to root
+		root_anno_scores = cbind(root_anno, in_genes[match(root_anno[,2], in_genes[,1]), 2:ncol(in_genes)])
+
+		# aggregate scores in root nodes
+		if (test != "wilcoxon"){
+			if (test == "hyper"){ 
+				# counts of 1 and 0 genes in a node
+				root_anno_scores[is.na(root_anno_scores[,3]), 3] = 0 
+				root_anno_scores = tapply(root_anno_scores[,3], root_anno_scores[,1], function(x) c(sum(x[]), length(x)-sum(x)))
+				root_anno_scores = data.frame(go_id = names(root_anno_scores), do.call(rbind, root_anno_scores))
+			} else { 
+				# sums of scores in a node (binom + conti)
+				root_anno_scores = aggregate(root_anno_scores[,3:ncol(root_anno_scores)], list(go_id=root_anno_scores[,1]), sum)
+			}
+		}
+		
+		# add colors and root_node_name
+		root_anno_scores$root_name = get_names(root_anno_scores[,1])[,2]
+		root_anno_scores$root_col = root_cols[match(root_anno_scores[,1], root_cols[,1]), 2]
+		# merge nodes with root node info
+		matched_root_name = get_names(anno_scores[,1])[,3] # get names
+		anno_scores$root_id = root_names[match(matched_root_name, root_names[,2]), 4]
+		anno_scores = cbind(anno_scores, root_anno_scores[match(anno_scores$root_id, root_anno_scores[,1]), 2:ncol(root_anno_scores)])
+	}
+
+	
+	# recover original order (aggregate and get_anno_genes sorts output alphabetically)
+	anno_scores = anno_scores[order(ordere[match(anno_scores$go_id, ordere$go_ids), 2]),]
+	rownames(anno_scores) = 1:nrow(anno_scores)
+
 	# plot and get stats returned
 	if (test == "hyper"){
-		stats = plot_hyper(aggrego, root_aggrego)
+		stats = plot_hyper(anno_scores, root_anno_scores)
 	} else if (test == "binomial"){
-		stats = plot_binomial(aggrego, root_aggrego)
+		stats = plot_binomial(anno_scores, root_anno_scores)
 	} else if (test == "contingency"){
-		stats = plot_conti(aggrego) # TODO, maybe avoid root_aggrego for contingency
+		stats = plot_conti(anno_scores) # TODO, maybe avoid root_anno_scores for contingency
 	}
 	
 	# wilcoxon:
