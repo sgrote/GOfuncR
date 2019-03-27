d2b65d08f2d7d197751ed386945b7dbf0d0687e9
Steffi <steffi_grote@eva.mpg.de>
2019-03-27 11:25:13 +0100

    remove db-version dependent tests

--- tests/testthat/test_go_enrich.R
+++ tests/testthat/test_go_enrich.R
@@ -20,9 +20,6 @@ input_hyper_bg = data.frame(gene_ids = c(candi_gene_ids, bg_gene_ids), is_candid
 res_hyper_bg = go_enrich(input_hyper_bg, n_randsets=20, silent=TRUE)
 
 test_that("hyper_defined_bg works fine",{
-	expect_true("GO:0005634" %in% res_hyper_bg[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_hyper_bg[[1]][1,"FWER_underrep"] > 0.9)
-	expect_true(res_hyper_bg[[1]][1,"raw_p_overrep"] < 0.5)
 	expect_true(setequal(root_names, unique(res_hyper_bg[[1]][,1])))
 	expect_true(setequal(res_hyper_bg[[2]][,2], c(0, 1)))
 	expect_true(nrow(res_hyper_bg[[3]]) == 2)
@@ -35,9 +32,6 @@ set.seed(123)
 res_hyper_len = go_enrich(input_hyper_bg, gene_len=TRUE, n_randsets=50, silent=TRUE)
 
 test_that("hyper_gene_len works fine",{
-	expect_true("GO:0005634" %in% res_hyper_len[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_hyper_len[[1]][1,"FWER_underrep"] > 0.9)
-	expect_true(res_hyper_len[[1]][1,"FWER_overrep"] < 1)
 	expect_true(setequal(root_names, unique(res_hyper_len[[1]][,1])))
 	expect_true(setequal(res_hyper_len[[2]][,2], c(0, 1)))
 	expect_equivalent(res_hyper_len[[3]][1,1:2], c("go_annotations","Homo.sapiens"))
@@ -53,11 +47,7 @@ input_regions = data.frame(regions, is_candidate)
 res_region = go_enrich(input_regions, n_randsets=30, regions=TRUE, silent=TRUE)
 
 test_that("hyper_regions works fine",{
-	expect_true("GO:0019433" %in% res_region[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_region[[1]][1,"FWER_underrep"] > 0.5)
-	expect_true(res_region[[1]][1,"FWER_overrep"] < 0.5)
 	expect_true(setequal(root_names, unique(res_region[[1]][,1])))
-	expect_true(nrow(res_region[[2]]) > 1000)
 	expect_true(setequal(res_region[[2]][,2], c(0, 1)))
 	expect_equivalent(res_region[[3]][1,1:2], c("go_annotations","Homo.sapiens"))
 	expect_equivalent(res_region[[3]][2,1:2], c("gene_coordinates","Homo.sapiens"))
@@ -72,11 +62,7 @@ set.seed(123)
 test_that("hyper_regions_circ works fine",{
 	expect_warning((res_circ = go_enrich(input_regions[c(1,3:5),], n_randsets=20, regions=TRUE, silent=TRUE, circ_chrom=TRUE)),
 	    "Unused chromosomes in background regions: chr7.\n  With circ_chrom=TRUE only background regions on the same chromosome as a candidate region are used.")
-	expect_true("GO:0019433" %in% res_circ[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_circ[[1]][1,"FWER_underrep"] > 0.5)
-	expect_true(res_circ[[1]][1,"FWER_overrep"] < 0.5)
 	expect_true(setequal(root_names, unique(res_circ[[1]][,1])))
-	expect_true(nrow(res_circ[[2]]) < 1000 && nrow(res_circ[[2]]) > 500)
 	expect_true(setequal(res_circ[[2]][,2], c(0, 1)))
 	expect_equivalent(res_circ[[3]][1,1:2], c("go_annotations","Homo.sapiens"))
 	expect_equivalent(res_circ[[3]][2,1:2], c("gene_coordinates","Homo.sapiens"))
@@ -92,10 +78,6 @@ input_willi = data.frame(gene_ids = c(high_score_genes, low_score_genes), gene_s
 res_willi = go_enrich(input_willi, test='wilcoxon', n_randsets=20, silent=TRUE)
 
 test_that("wilcox works fine",{
-	expect_true("GO:0098793" %in% res_willi[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_willi[[1]][1,"FWER_low_rank"] == 1)
-	expect_true(res_willi[[1]][1,"FWER_high_rank"] < 0.5)
-	expect_true(res_willi[[1]][1,"raw_p_high_rank"] < 0.05)
 	expect_true(setequal(root_names, unique(res_willi[[1]][,1])))
 	expect_true(all(res_willi[[2]][,2] %in% gene_scores)) # only score for QUATSCH is missing 
 	expect_true(nrow(res_willi[[3]]) == 2)
@@ -113,10 +95,6 @@ input_binom = data.frame(gene_ids=c(high_A_genes, low_A_genes), A_counts, B_coun
 res_binom = go_enrich(input_binom, test='binomial', n_randsets=20, silent=TRUE)
 
 test_that("binom works fine",{
-	expect_true("GO:0005536" %in% res_binom[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_binom[[1]][1,"FWER_high_B"] == 1)
-	expect_true(res_binom[[1]][1,"FWER_high_A"] < 0.3)
-	expect_true(res_binom[[1]][1,"raw_p_high_A"] < 0.0005)
 	expect_true(setequal(root_names, unique(res_binom[[1]][,1])))
 	expect_true(all(res_binom[[2]][,2] %in% A_counts)) 
 	expect_true(all(res_binom[[2]][,3] %in% B_counts)) 
@@ -137,10 +115,6 @@ input_conti = data.frame(gene_ids=c(high_substi_genes, low_substi_genes),
 res_conti = go_enrich(input_conti, test='contingency', n_randsets=20, silent=TRUE)
 
 test_that("conti works fine",{
-	expect_true("GO:0005829" %in% res_conti[[1]][1:100,2]) # should be high independent of rannum/versions
-	expect_true(res_conti[[1]][1,"FWER_high_CD"] == 1)
-	expect_true(res_conti[[1]][1,"FWER_high_AB"] < 0.7)
-	expect_true(res_conti[[1]][1,"raw_p_high_AB"] < 0.0005)
 	expect_true(setequal(root_names, unique(res_conti[[1]][,1])))
 	expect_true(all(res_conti[[2]][,2] %in% subs_non_syn)) 
 	expect_true(all(res_conti[[2]][,3] %in% subs_syn)) 
